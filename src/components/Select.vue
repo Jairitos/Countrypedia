71a07ecc06a09e0c99e5118d7a0de07587e18146
Djeiro <gebruiker@MacBook-Pro-van-Gebruiker.local>
2020-09-28 09:59:59 +0200

    added more styling + changes to select filter

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,20 +1,65 @@
 <template>
     <div>
-        <div @click="selectDropdown" class="w-full border border-gray-500 rounded h-full">
-            <div class="h-full flex items-center justify-between">
-                <h2 class="p-4">{{selected}}</h2>
-                <button v-show="selected !== 'Sort countries'" @click="clearSelection($event)" type="button" class="pr-4">X</button>
+        <div
+            class="country-filter w-full border border-gray-500 rounded bg-white overflow-hidden"
+        >
+            <div
+                class="selected-filter-container h-full flex items-center justify-between p-2 cursor-pointer"
+                @click="selectDropdown"
+                v-click-outside="handleFocusOut"
+            >
+                <label class="cursor-pointer" :class="[selected === 'Sort countries' ? 'text-gray-500 opacity-75'  : '']" v-html="selected"></label>
+                <div class="flex">
+                    <button
+                    v-show="selected !== 'Sort countries'"
+                    @click="clearSelection($event)"
+                    type="button"
+                    class="focus:outline-none"
+                >
+                    <delete-svg class="w-4 h-4 red-delete-icon" />
+                </button>
+                <img src="../assets/images/arrowhead.png" class="arrowhead ml-6 w-3 h-auto" :class="dropdownOpen ? 'rotate' : '' " />
+                </div>
             </div>
-            <div v-show="dropdownOpen" class="flex flex-col w-full">
-                <input @click="onChange($event)" value="Descending alphabetical" type="text" class="w-full py-2">
-                <input @click="onChange($event)" value="Most letters" type="text" class="w-full py-2">
+            <div v-show="dropdownOpen" class="flex flex-col w-full rounded">
+                <input
+                    @click="onChange($event)"
+                    type="text"
+                    value="Descending alphabetical"
+                    :disabled="selected === 'Descending alphabetical'"
+                    class="option w-full p-2 hover:bg-mist-blue cursor-pointer focus:outline-none"
+                />
+                <input
+                    @click="onChange($event)"
+                    :disabled="selected === 'Most letters'"
+                    type="text"
+                    value="Most letters"
+                    class="option w-full p-2 hover:bg-mist-blue cursor-pointer focus:outline-none"
+                />
             </div>
         </div>
     </div>
-</template>ƒ
+</template>
 
 <script lang="ts">
 import Vue from "vue";
+
+Vue.directive("click-outside", {
+    bind: function (el, binding, vnode) {
+        el.clickOutsideEvent = function (event) {
+            // here I check that click was outside the el and his childrens
+            if (!(el == event.target || el.contains(event.target))) {
+                // and if it did, call method provided in attribute value
+                vnode.context[binding.expression](event);
+            }
+        };  
+        document.body.addEventListener("click", el.clickOutsideEvent);
+    },
+    unbind: function (el) {
+        document.body.removeEventListener("click", el.clickOutsideEvent);
+    },
+});
+
 export default Vue.extend({
     data() {
         return {
@@ -24,10 +69,12 @@ export default Vue.extend({
     },
     methods: {
         emitSelectedOption() {
-            this.$emit('add:selectedOption', this.selected);
+            this.$emit("add:selectedOption", this.selected);
         },
         onChange(event: any) {
+            this.clearActiveClass();
             this.selected = event.target.value;
+            event.target.classList.add("active");
             this.emitSelectedOption();
         },
         selectDropdown() {
@@ -36,8 +83,21 @@ export default Vue.extend({
         clearSelection(event: any) {
             event.stopImmediatePropagation();
             this.selected = "Sort countries";
+            this.dropdownOpen = false;
+            this.clearActiveClass();
             this.emitSelectedOption();
-        }
+        },
+        clearActiveClass() {
+            const options = document.querySelectorAll(".option") as NodeListOf<
+                HTMLBodyElement
+            >;
+            for (const option of options) {
+                option.classList.remove("active");
+            }
+        },
+        handleFocusOut() {
+            this.dropdownOpen = false;
+        },
     },
 });
 </script>
